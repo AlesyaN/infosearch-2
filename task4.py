1194f3bcf20d0e0b336753475826f956e425f356
Lesya <sikamikanico9494@gmail.com>
2021-04-05 10:50:26 +0300

    bugfix

--- task4.py
+++ task4.py
@@ -2,7 +2,6 @@ import json
 import math
 from nltk.tokenize import word_tokenize
 
-
 def tf_idf(dataLines):
     result = {}
     for jsonData in dataLines:
@@ -30,15 +29,31 @@ def count_tf(data):
             occurrence_num = 0
             words_num = 0
             for line in lines:
-                occurrence_num += line.count(data.get('word'))
-                words_num += len(word_tokenize(line, language="russian"))
+                tokens_list = word_tokenize(line, language="russian")
+                occurrence_num += count_words_in_list_by_lemma(tokens_list, data.get('word'))
+                words_num += len(tokens_list)
             result_for_word[doc_num] = occurrence_num / words_num
     return result_for_word
+
+
+def count_words_in_list_by_lemma(list, lemma):
+    count = 0
+    words_for_lemma = lemmas.get(lemma)
+    for token in list:
+        if words_for_lemma.__contains__(token):
+            count += 1
+    return count
 
 
 def count_idf(data):
     return math.log(100 / data.get('count'))
 
+with open('./result/lemmas.txt', 'r', encoding='utf-8') as lemmas_file:
+    lemmas = {}
+    lemmmas_lines = lemmas_file.readlines()
+    for line in lemmmas_lines:
+        words = line.split()
+        lemmas[words[0]] = words
 
 with open('./resources/invertedIndex.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
